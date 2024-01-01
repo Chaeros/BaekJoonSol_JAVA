7762a576d3f070dda4f2d0f23160dbcde9f3181b
Chaeros <chpark6851@koreatech.ac.kr>
2024-01-01 22:09:36 +0900

    boj: 1day 1sol

---
+++ src/삼성기출/BOJ19236.java
@@ -0,0 +1,24 @@
+package 삼성기출;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BOJ19236 {
+
+    static ArrayList<ArrayList<ArrayList<Shark>>> map = new ArrayList<>();
+
+    static class Shark{
+        int number;
+        int direction;
+        
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+
+    }
+}
